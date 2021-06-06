9a8f9627a40cbd77fb117e0cdfb25d80ca883cea
Bizblk <enemy_counter@mail.ru>
2021-06-06 10:18:33 +0300

    Initial Commit

---
+++ RGBSliderSwiftUI/RGBSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSliderSwiftUIApp.swift
+//  RGBSliderSwiftUI
+//
+//  Created by Виталий Оранский on 06.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
